eabf2e3ba1734ddf9f668375eb147c88bdf4775b
Brendan Berg <brendan_berg@aol.com>
2019-02-13 19:58:21 +0100

    Add pin direction handling to corresponding class
    The configuration for the data direction for a digital pin was
    outsourced to the corresponding DigitalOutputPin class.

--- io/DigitalOutputPin.cpp
+++ io/DigitalOutputPin.cpp
@@ -10,6 +10,7 @@ DigitalOutputPin::DigitalOutputPin (volatile uint8_t* port, uint8_t bit)
 {
     m_Mask = static_cast<uint8_t>(1) << bit;
     m_MaskInv = ~m_Mask;
+    *(m_Port - 1) |=  m_Mask;
 }
 
 
